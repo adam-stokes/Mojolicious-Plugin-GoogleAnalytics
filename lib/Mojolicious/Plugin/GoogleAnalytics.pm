4913a5e0966f4501543c5e6b0787de7282efca19
Adam Stokes <adam.stokes@ubuntu.com>
2013-06-22 00:23:23 -0400

    initial script code
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Mojolicious/Plugin/GoogleAnalytics.pm
+++ lib/Mojolicious/Plugin/GoogleAnalytics.pm
@@ -1,10 +1,37 @@
 package Mojolicious::Plugin::GoogleAnalytics;
+
+use strictures 1;
 use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::ByteStream 'b';
 
 # VERSION
 
 sub register {
-  my ($self, $app) = @_;
+    my ($self, $app) = @_;
+
+    $app->helper(
+        analytics => sub {
+            my $c            = shift;
+            my $analytics_id = shift;
+
+            die "No analytics ID defined" unless exists $analytics_id;
+	    return b(<<"ENDANALYTICS")->squish;
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '${analytics_id}']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+ENDANALYTICS
+        }
+    );
 }
 
 1;
@@ -22,9 +49,14 @@ Mojolicious::Plugin::GoogleAnalytics - Mojolicious Plugin
   # Mojolicious::Lite
   plugin 'GoogleAnalytics';
 
+  # In your layout template
+  <%= analytics 'UA-32432-1' %>
+  </body> <!-- Make sure its just before closing body tag -->
+
+
 =head1 DESCRIPTION
 
-L<Mojolicious::Plugin::GoogleAnalytics> is a L<Mojolicious> plugin.
+L<Mojolicious::Plugin::GoogleAnalytics> is a L<Mojolicious> plugin. Inserts Google Analytics code and associates your analytics id.
 
 =head1 METHODS
 
