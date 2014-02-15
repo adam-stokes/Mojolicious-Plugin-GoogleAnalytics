4a3bf61fba6fe9c1ae3b4f8a64df51c6125b04aa
Adam Stokes <adam.stokes@ubuntu.com>
2014-02-15 13:43:39 -0500

    prep before release
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Mojolicious/Plugin/GoogleAnalytics.pm
+++ lib/Mojolicious/Plugin/GoogleAnalytics.pm
@@ -1,11 +1,50 @@
 package Mojolicious::Plugin::GoogleAnalytics;
+# ABSTRACT: GoogleAnalytics plugin
+
+=head1 SYNOPSIS
+
+  # Mojolicious
+  $self->plugin('GoogleAnalytics');
+
+  # Mojolicious::Lite
+  plugin 'GoogleAnalytics';
+
+  # In your layout template
+  <%= analytics_inc 'UA-32432-1', 'example.com', 1 %>
+  </head> <!-- Make sure its just before closing head tag -->
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::GoogleAnalytics> is a L<Mojolicious>
+plugin. Inserts Google Analytics code and associates your analytics
+id.
+
+=head1 OPTIONS
+
+=head2 Track Subdomains
+
+Put the domain which qualifies any subdomains you wish to track,
+eg. blog.example.com, apps.example.com will have the second arguement
+set to 'example.com'
+
+=head2 Multiple top level domains
+
+Default is set to 1 to allow domains such as example.fr, example.cn,
+and example.com
+
+=cut
 
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = '1.004';
-
 has 'template' => 'analytics_template';
 
+=method register
+
+  $plugin->register(Mojolicious->new);
+
+Register plugin in L<Mojolicious> application.
+
+=cut
 sub register {
     my ($plugin, $app) = (shift, shift);
     push @{$app->renderer->classes}, __PACKAGE__;
@@ -55,64 +94,3 @@ __DATA__
   })();
 %= end
 
-__END__
-
-=head1 NAME
-
-Mojolicious::Plugin::GoogleAnalytics - Mojolicious Plugin
-
-=head1 SYNOPSIS
-
-  # Mojolicious
-  $self->plugin('GoogleAnalytics');
-
-  # Mojolicious::Lite
-  plugin 'GoogleAnalytics';
-
-  # In your layout template
-  <%= analytics_inc 'UA-32432-1', 'example.com', 1 %>
-  </head> <!-- Make sure its just before closing head tag -->
-
-
-=head1 DESCRIPTION
-
-L<Mojolicious::Plugin::GoogleAnalytics> is a L<Mojolicious> plugin. Inserts Google Analytics code and associates your analytics id.
-
-=head1 METHODS
-
-L<Mojolicious::Plugin::GoogleAnalytics> inherits all methods from
-L<Mojolicious::Plugin> and implements the following new ones.
-
-=head2 register
-
-  $plugin->register(Mojolicious->new);
-
-Register plugin in L<Mojolicious> application.
-
-=head1 OPTIONS
-
-=head2 Track Subdomains
-
-Put the domain which qualifies any subdomains you wish to track, eg. blog.example.com, apps.example.com will have the second arguement set to 'example.com'
-
-=head2 Multiple top level domains
-
-Default is set to 1 to allow domains such as example.fr, example.cn, and example.com
-
-=head1 AUTHOR
-
-Adam Stokes E<lt>adamjs@cpan.orgE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2013- Adam Stokes
-
-=head1 LICENSE
-
-Licensed under the same terms as Perl.
-
-=head1 SEE ALSO
-
-L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicio.us>.
-
-=cut
